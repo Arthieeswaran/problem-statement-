95aa298ce50d3a455f6d489be51068696fabf648
Arthieeswaran <159516841+Arthieeswaran@users.noreply.github.com>
2024-10-01 15:00:39 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,14 +1,53 @@
-let m=1;
-let n=3;
-let sum=0;
-if(m>n)
-{
-    console.log(0);
+console.log("MENU");
+console.log("1. Pizza");
+console.log("2. Burger");
+console.log("3. Biriyani");
+console.log("4. Apple juice");
+console.log("5. Idli");
+
+let choice = prompt("Enter your choice");
+let rate = 0; 
+let n, cost; 
+
+switch(choice) {
+    case "1": 
+        n = prompt("Enter the required quantity");
+        cost = 200;
+        rate = cost * n;
+        break;
+
+    case "2": 
+        n = prompt("Enter the required quantity");
+        cost = 300;
+        rate = cost * n;
+        break;
+
+    case "3": 
+        n = prompt("Enter the required quantity");
+        cost = 300;
+        rate = cost * n;
+        break;
+
+    case "4": 
+        n = prompt("Enter the required quantity");
+        cost = 250;
+        rate = cost * n;
+        break;
+
+    case "5": 
+        n = prompt("Enter the required quantity");
+        cost = 100;
+        rate = cost * n;
+        break;
+
+    default:
+        console.log("Invalid choice");
+        break;
 }
-else{
-    for(let i=m;i<=n;i++)
-    {
-        sum+=i*i*i;
-    }
-    console.log(sum);
+
+if(rate > 500){
+    rate = rate - (rate * (10 / 100)); 
+    console.log( rate);
+} else {
+    console.log( rate);
 }
